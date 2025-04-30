8cad7a7450e4ce86b36311c1f4e48614f94a085c
Jim <jimsamuel50@gmail.com>
2025-04-30 21:25:03 +0100

    estorage . evm tests

--- src/evm/storage.rs
+++ src/evm/storage.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 use primitive_types::U256;
 
-pub struct _Ememory{
+pub struct _Estorage{
     data: HashMap<U256, U256>
 }
 
-impl _Ememory { 
+impl _Estorage { 
     pub fn _new() ->Self {
         Self {data: HashMap::new()}
     }
@@ -22,5 +22,29 @@ impl _Ememory {
 
     pub fn _dump(&self) -> &HashMap<U256, U256> {
         &self.data
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use primitive_types::U256;
+
+    #[test]
+    fn test_store_and_load(){
+        let mut storage = _Estorage::_new();
+        let key = U256::from(0x1);
+        let val = U256::from(0xabc);
+        storage._store(key, val);
+        assert_eq!(storage._load(key), val);
+    }
+
+    #[test]
+    fn test_storage_zero_deletes() {
+        let mut storage = _Estorage::_new();
+        let key = U256::from(0x02);
+        storage._store(key, U256::from(0x233));
+        storage._store(key, U256::zero());
+        assert_eq!(storage._load(key), U256::zero());
     }
 }
