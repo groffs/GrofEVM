c849afdcf94b50c6f5c4c191b9ab17bc176ac766
Jim <jimsamuel50@gmail.com>
2025-04-30 21:33:33 +0100

    stack evm test

--- src/evm/stack.rs
+++ src/evm/stack.rs
@@ -1,7 +1,7 @@
-
+use primitive_types::U256;
 
 pub struct _Stack {
-    data:Vec<u128>
+    data:Vec<U256>
 }
 
 impl _Stack {
@@ -9,7 +9,7 @@ impl _Stack {
         Self {data: Vec::with_capacity(1024)}
     }
 
-    pub fn _push(&mut self, val: u128) {
+    pub fn _push(&mut self, val: U256) {
         if self.data.len() >= 1024 {
             panic!("stack overflow");
         }
@@ -18,5 +18,22 @@ impl _Stack {
 
     pub fn _pop(&mut self) {
         self.data.pop().expect("stack underflow");
+    }
+
+    pub fn _dump(&self) -> &Vec<U256>{
+        &self.data
+    }   
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn test_stack_addition_load() {
+        let mut stack = _Stack::_new();
+        let val = U256::from(0x02);
+        stack._push(val);
+        assert_eq!(stack._dump(), &vec![val]);
     }
 }
