a5b588eb4ad0ec088f50d9567f054fc4065e3be2
Jim <jimsamuel50@gmail.com>
2025-05-04 11:02:10 +0100

    enum to explicitly specify opcode

--- src/evm/opcode.rs
+++ src/evm/opcode.rs
@@ -1,6 +1,15 @@
 use primitive_types::U256;
+use sha2::digest::consts::U25;
 
 use super::stack::Stack;
+
+pub enum Opcode {
+    Add,
+    Mul,
+    Exp,
+    SignExtend,
+    CallDataCopy
+}
 
 #[derive(Debug)]
 pub enum EvmError {
@@ -11,9 +20,9 @@ pub enum EvmError {
 pub type EvmResult<T> = Result<T, EvmError>;
 
 
-pub fn execute(opcode : u8, stack: &mut Stack, gas: U256) -> EvmResult<()>{
-    match opcode {
-        0x01 => {
+pub fn execute(evmop : Opcode, stack: &mut Stack, gas: U256) -> EvmResult<()>{
+    match evmop {
+        Opcode::Add => {
             /* handles the addition operation for the evm */
             /* handle the two error cases that could happen for this call */
             /* it's either insufficient gas or stack underflow */
@@ -24,17 +33,17 @@ pub fn execute(opcode : u8, stack: &mut Stack, gas: U256) -> EvmResult<()>{
             stack.push(stack_top.unwrap() + stack_bot.unwrap());
             Ok(())
         },
-        0x02 => {
+        Opcode::Mul => {
             /* handles the multiplication operation for the evm */
             /* the amount of gas needed for each operation is different */
             if gas < U256::from(5) { return Err(EvmError::OutOfGas);}
             if stack.data.len() < 2 { return Err(EvmError::StackUnderflow);}
             let stack_top = stack.pop().ok_or(EvmError::StackUnderflow);
             let stack_bot = stack.pop().ok_or(EvmError::StackUnderflow);
-            stack.push(stack_top.unwrap() +  stack_bot.unwrap());
+            stack.push(stack_top.unwrap() *  stack_bot.unwrap());
             Ok(())
         },
-        0x0A => {
+        Opcode::Exp => {
             if gas < U256::from(10) { return Err(EvmError::OutOfGas);}
             if stack.data.len() < 2 { return Err(EvmError::StackUnderflow);}
             let stack_exp = stack.pop().ok_or(EvmError::StackUnderflow);
@@ -43,7 +52,27 @@ pub fn execute(opcode : u8, stack: &mut Stack, gas: U256) -> EvmResult<()>{
             stack.push(operation);
             Ok(())
         },
-        0x0B => {
+        Opcode::CallDataCopy => {
+            /* handles the calldatacopy operation for the evm */
+            /* the calldatacopy call has the gas formula below */
+            /* Gverylow + Gcopy × dµs[2] ÷ 32e */
+            let very_low = stack.pop().ok_or(EvmError::StackUnderflow)?;
+            let copy_gas = stack.pop().ok_or(EvmError::StackUnderflow)?;
+            let cdsize = stack.pop().ok_or(EvmError::StackUnderflow)?;
+            let mod_stack = cdsize % 32;
+
+            /* compute the gas cost for the call data copy and */
+            /* determine if the caller has enough gas for the op */
+            if U256::from(mod_stack) > gas /* could also mean gas < mod_stack */ {
+                /* let's see the amount of gas passed at this point */
+                dbg!(gas); dbg!(cdsize);
+                return Err(EvmError::OutOfGas);
+            }
+            let stack_ops = very_low + (copy_gas * mod_stack );
+            stack.push(stack_ops);
+            Ok(())
+        },
+        Opcode::SignExtend => {
             /* sign extension which is used for immediate register calls */
             /* and specifically used for negative value conversions based */
             /* on two-s complement */
