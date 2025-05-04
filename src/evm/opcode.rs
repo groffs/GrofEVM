f81a49aa4d351598303e65a932998ee35c112c7f
Jim <jimsamuel50@gmail.com>
2025-05-04 10:47:38 +0100

    evm exponent operation

--- src/evm/opcode.rs
+++ src/evm/opcode.rs
@@ -12,18 +12,44 @@ pub type EvmResult<T> = Result<T, EvmError>;
 
 
 pub fn execute(opcode : u8, stack: &mut Stack, gas: U256) -> EvmResult<()>{
-    
-    if gas < U256::from(3) { 
-        return Err(EvmError::OutOfGas);
-    }
-
     match opcode {
         0x01 => {
+            /* handles the addition operation for the evm */
+            /* handle the two error cases that could happen for this call */
+            /* it's either insufficient gas or stack underflow */
+            if gas < U256::from(3)  { return Err(EvmError::OutOfGas);}
+            if stack.data.len() < 2 { return Err(EvmError::StackUnderflow); }
             let stack_top = stack.pop().ok_or(EvmError::StackUnderflow);
             let stack_bot = stack.pop().ok_or(EvmError::StackUnderflow);
             stack.push(stack_top.unwrap() + stack_bot.unwrap());
             Ok(())
         },
+        0x02 => {
+            /* handles the multiplication operation for the evm */
+            /* the amount of gas needed for each operation is different */
+            if gas < U256::from(5) { return Err(EvmError::OutOfGas);}
+            if stack.data.len() < 2 { return Err(EvmError::StackUnderflow);}
+            let stack_top = stack.pop().ok_or(EvmError::StackUnderflow);
+            let stack_bot = stack.pop().ok_or(EvmError::StackUnderflow);
+            stack.push(stack_top.unwrap() +  stack_bot.unwrap());
+            Ok(())
+        },
+        0x0A => {
+            if gas < U256::from(10) { return Err(EvmError::OutOfGas);}
+            if stack.data.len() < 2 { return Err(EvmError::StackUnderflow);}
+            let stack_exp = stack.pop().ok_or(EvmError::StackUnderflow);
+            let stack_val = stack.pop().ok_or(EvmError::StackUnderflow);
+            let operation = stack_val.unwrap() ^ stack_exp.unwrap();
+            stack.push(operation);
+            Ok(())
+        },
+        0x0B => {
+            /* sign extension which is used for immediate register calls */
+            /* and specifically used for negative value conversions based */
+            /* on two-s complement */
+           
+            Ok(())
+        }
         _ => panic!("bad opcode")
     }
 }
@@ -37,6 +63,6 @@ mod test {
         let eops = 0x01;
         let gas = U256::from(4);
         let mut stack = Stack::new();
-        execute(eops, &mut stack, gas);
+        execute(eops, &mut stack, gas).unwrap();
     }
 }
